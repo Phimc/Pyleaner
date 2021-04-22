24eef3e9e34c68a94581684728921bcf6ab84ac9
Phimc <41496326+Phimc@users.noreply.github.com>
2021-04-22 23:36:44 +0800

    Update OJ5.py

--- OJ5.py
+++ OJ5.py
@@ -1,24 +1,23 @@
 M = int(input())
-essay = input().strip(".-?!',").lower().split()
+essay = input().split()
 memo = []
 searchtime = 0
 le = 0
 dot = [".","-","?","!","'","," ]
 for word in essay:
-    if word in dot:
-        continue
-    word = word.strip(".-?!',")
-    if word in memo:
-        le += 1
-        continue
-    elif len(memo) <= M-1:
-        memo.append(word)
-        searchtime += 1
-        le += 1
-    else:
-        memo.pop(0)
-        memo.append(word)
-        searchtime += 1
-        le += 1
+    word = word.strip(".-?!',").lower()
+    if word != '':
+        if word in memo:
+            le += 1
+            continue
+        elif len(memo) <= M-1:
+            memo.append(word)
+            searchtime += 1
+            le += 1
+        else:
+            memo.pop(0)
+            memo.append(word)
+            searchtime += 1
+            le += 1
 print(le)
 print(searchtime)
